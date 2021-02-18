f65feb0cafbbe6700b1d7329fed3a1d8eda5e34e
psm651 <psm651@naver.com>
2021-02-18 23:08:06 +0900

    JDBC연결 추가

--- src/main/java/com/hello/hello/SpringConfig.java
+++ src/main/java/com/hello/hello/SpringConfig.java
@@ -1,14 +1,24 @@
 package com.hello.hello;
 
+import com.hello.hello.repository.JdbcMemberRepository;
 import com.hello.hello.repository.MemberRepository;
 import com.hello.hello.repository.MemoryMemberRepository;
 import com.hello.hello.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +26,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+
     }
+
 }
