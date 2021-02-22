579f52743907742fd0a2998555fb7fe1d1abc393
psm651 <psm651@naver.com>
2021-02-22 22:41:25 +0900

    스프링 JPA

--- src/main/java/com/hello/hello/SpringConfig.java
+++ src/main/java/com/hello/hello/SpringConfig.java
@@ -11,12 +11,18 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
 
-    private EntityManager em;
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+//    private EntityManager em;
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     //    private DataSource dataSource;
 //
@@ -26,16 +32,16 @@ public class SpringConfig {
 //    }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+    //@Bean
+    //public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+       // return new JpaMemberRepository(em);
 
-    }
+    //}
 
 }
---
+++ src/main/java/com/hello/hello/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package com.hello.hello.repository;
+
+import com.hello.hello.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
